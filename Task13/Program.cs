36109435d87549ae662ef10d822c9c44f1128385
Nataliya <arnicaaliya@gmail.com>
2022-12-25 23:47:26 +0300

    ifElseвыводимТретьюЦифру

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// вывод третьей цифры введенного числа.Либо сообщить,что третьей цифры нет.
+Console.WriteLine("Введите число");
+String number= Console.ReadLine();
+
+if (number.Length>=3)
+{
+   Console.Write("третья цифра"+"=");
+   Console.WriteLine(number[2]); 
+}
+else
+{
+    Console.Write ("третьей цифры нет");
+}
