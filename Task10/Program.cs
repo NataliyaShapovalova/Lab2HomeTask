4cc7d6582217cfb6afbeeb47042e6d2871df689b
Nataliya <arnicaaliya@gmail.com>
2022-12-25 22:24:16 +0300

    число разделили на 10ивывели остаток от деления на 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.Write("Введите трехзначное число:");
+﻿//Показать вторую цивру введенного трехзначного числа
+Console.Write("Введите трехзначное число:");
 int number= Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(number%100);
+System.Console.WriteLine(number/10%10);
